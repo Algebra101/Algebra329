31ed1501297fecf526cce1c24689f43729af0703
Olalere Robiu <105038790+Algebra101@users.noreply.github.com>
2023-12-04 11:34:08 +0000

    Making an update to a Template

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Robiu Olalere",
+  title: "Machine Learning Engineer",
+  email: "Olalererobiu67@gmail.com",
+  gitHub: "Olalere Robiu",
+  instagram: "olalere_robiu",
+  linkedIn: "algebra329",
+  twitter: "Algebra",
+  youTube: "Robiu Olalere",
 };
 
 const primaryColor = "#4E567E";
